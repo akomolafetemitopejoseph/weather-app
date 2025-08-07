06433bd8c52c00d702d08d327e5f128cbb8916e1
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-08-07 09:18:46 -0700

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-blue-100">
+      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full">
+        <h1
+          className="text-3
+       text-center font-bold"
+        >
+          Weather app
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
